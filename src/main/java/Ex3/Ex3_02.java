1cb384d198eaee3e4fcdbe9ccedc66bfe6f3e138
Leedonguk <donguk0105@naver.com>
2023-03-18 00:22:40 +0900

    교제 문제 풀이

---
+++ src/main/java/Ex3/Ex3_02.java
@@ -0,0 +1,12 @@
+package Ex3;
+
+public class Ex3_02 {
+    public static void main(String[] args) {
+        int result;
+
+        System.out.printf("%d + %d = %d", 100, 200, 300);
+        System.out.printf("\n");
+        System.out.printf("%d / %d = %f", 100, 200, 0.5);
+        System.out.printf("\n");
+    }
+}
