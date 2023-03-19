ad5598604dbc1adf874f8cb3c857fd3dd328317b
Leedonguk <donguk0105@naver.com>
2023-03-19 22:43:47 +0900

    백준 문제

---
+++ src/main/java/Baekjoon/_1/B_1_1000.java
@@ -0,0 +1,16 @@
+package Baekjoon._1;
+
+import java.util.Scanner;
+
+public class B_1_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a + b);
+    }
+}
---
+++ src/main/java/Baekjoon/_1/B_1_2557.java
@@ -0,0 +1,7 @@
+package Baekjoon._1;
+
+public class B_1_2557 {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
