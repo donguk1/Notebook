5ec8feb173d4ff06991bbbefe71e35e4935da57e
Leedonguk <donguk0105@naver.com>
2023-03-21 21:47:49 +0900

    백준

--- src/main/java/Baekjoon/_1/B_1_1008.java
+++ src/main/java/Baekjoon/_1/B_1_1008.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class B_1_1008 {
     public static void main(String[] args) {
         int a, b;
-        int result;
+        double result;
 
         Scanner s = new Scanner(System.in);
 
         a = s.nextInt();
         b = s.nextInt();
 
+        result = (double) a / b;
 
-        //System.out.println("%d / %d = %.10f", a, b, result);
-
-        //System.out.println("%d / %d = %f", a, b, result);
+        System.out.println(result);
     }
 }
