437d215f484514c0bdf8a9415c582441f8c0ef78
Leedonguk <donguk0105@naver.com>
2023-03-25 23:32:45 +0900

    백준

---
+++ src/main/java/Baekjoon/_1/B_1_18108.java
@@ -0,0 +1,12 @@
+package Baekjoon._1;
+
+import java.util.Scanner;
+
+public class B_1_18108 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+
+        System.out.println(a - 543);
+    }
+}
