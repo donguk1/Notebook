506264545dc47dc9e317c5d55e1dbdef4c4e7124
Leedonguk <donguk0105@naver.com>
2023-03-30 13:56:47 +0900

    퀴즈 문제

---
+++ src/main/java/Quiz/Quiz_0330.java
@@ -0,0 +1,23 @@
+package Quiz;
+
+public class Quiz_0330 {
+    public static void main(String[] args) {
+
+        int i, k;
+        k = 2;
+
+        for (i = 1; i < 10; i++)
+        {
+            System.out.printf("%d * %d = %d \n", k, i, k*i);
+            if (i == 9){
+                k++;
+                i = 1;
+                System.out.println();
+            }
+            if (k == 10)
+                break;
+
+        }
+    }
+
+}
