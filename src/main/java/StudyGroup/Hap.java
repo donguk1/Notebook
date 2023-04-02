53ab8012a44e2a6337b7579ff8214f450eefc938
Leedonguk <donguk0105@naver.com>
2023-04-02 18:04:44 +0900

    스터디

---
+++ src/main/java/StudyGroup/Hap.java
@@ -0,0 +1,26 @@
+package StudyGroup;
+
+import java.util.Scanner;
+
+public class Hap {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int hap = 0;
+
+        if (a < b) {
+            for (; a <= b; a++) {
+                hap += a;
+            }
+        }
+        else if (a > b ){
+                for (; b <= a; b++) {
+                    hap += b;
+            }
+
+        }
+        System.out.print(hap);
+    }
+}
