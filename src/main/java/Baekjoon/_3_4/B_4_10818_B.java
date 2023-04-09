b93e3abda0d04c4509968605be5f1a470a188723
Leedonguk <donguk0105@naver.com>
2023-04-09 12:38:26 +0900

    23.04.09

---
+++ src/main/java/Baekjoon/_3_4/B_4_10818_B.java
@@ -0,0 +1,38 @@
+package Baekjoon._3_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_4_10818_B {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int arr[] = new int[n];
+
+        st = new StringTokenizer(br.readLine()," ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+
+        int max = -1000000;
+        int min = 1000000;
+
+        for (int i = 0; i < n; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        System.out.printf("%d %d", min, max);
+
+    }
+}
