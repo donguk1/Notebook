92bf7080a537a9368267978039cfc8cb2ee450dc
Leedonguk <donguk0105@naver.com>
2023-04-16 00:20:53 +0900

    23.04.16

---
+++ src/main/java/Baekjoon/_9/B_9_5086_B.java
@@ -0,0 +1,34 @@
+package Baekjoon._9;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B_9_5086_B {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a, b;
+
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            if ((a == 0) && (b == 0))
+                break;
+            if (b / a > 0 &&  b % a == 0) {
+                bw.write("factor\n");
+            } else if (a / b > 0 &&  a % b == 0) {
+                bw.write("multiple\n");
+            } else {
+                bw.write("neither\n");
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
